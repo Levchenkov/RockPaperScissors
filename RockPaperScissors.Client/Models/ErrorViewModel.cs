1dc6b056c4f116787fdaf9ae3a7879eccaecd787
Eugene Levchenkov <e.levchenkov@wannaplay.gg>
2022-02-15 11:51:37 +0300

    Init commit

---
+++ RockPaperScissors.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RockPaperScissors.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
