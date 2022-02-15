a813e923c410b115f00ec847a45a6f5a98380d38
Eugene Levchenkov <e.levchenkov@wannaplay.gg>
2022-02-15 13:42:33 +0300

    Add SignalR

---
+++ RockPaperScissors.Client/Hubs/MainHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RockPaperScissors.Client.Hubs;
+
+public class MainHub : Hub
+{
+    public async Task SelectPaper()
+    {
+        this.Clients.All.SendAsync("ShowResult");
+    }
+    
+    public async Task SelectRock()
+    {
+        this.Clients.All.SendAsync("ShowResult");
+    }
+    
+    public async Task SelectScissors()
+    {
+        this.Clients.All.SendAsync("ShowResult");
+    }
+}
--- RockPaperScissors.Client/wwwroot/js/site.js
+++ RockPaperScissors.Client/wwwroot/js/site.js
@@ -1,6 +1,18 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+var connection = new signalR.HubConnectionBuilder().withUrl("/mainHub").build();
+
+connection.on("ShowResult", function (user, message) {
+    console.log("show result");
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
 window.rock = "rock";
 window.paper = "paper";
 window.scissors = "scissors";
@@ -11,16 +23,28 @@ window.right = window.rock;
 function setLeftPaper(){
     window.left = window.paper;
     setLeftImageSrc("paper.png");
+
+    connection.invoke("SelectPaper").catch(function (err) {
+        return console.error(err.toString());
+    });
 }
 
 function setLeftRock(){
     window.left = window.rock;
     setLeftImageSrc("rock.png");
+
+    connection.invoke("SelectRock").catch(function (err) {
+        return console.error(err.toString());
+    });
 }
 
 function setLeftScissors(){
     window.left = window.scissors;
     setLeftImageSrc("scissors.png");
+
+    connection.invoke("SelectScissors").catch(function (err) {
+        return console.error(err.toString());
+    });
 }
 
 function setRightPaper(){
